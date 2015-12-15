05b590f949369b44b4a252075db25c711aa0632f
Matthew Lewis <matt@mplewis.com>
2015-12-15 00:23:42 -0600

    refactor: add serial, functionize

--- src/RGBShades.ino
+++ src/RGBShades.ino
@@ -5,66 +5,98 @@
 
 #define COLOR_ORDER RGB
 #define CHIPSET WS2811
-const byte ENTROPY_PIN = A5;
-const byte LED_PIN = 7;
-const byte BRIGHTNESS = 127;
+const char ENTROPY_PIN = A5;
+const char LED_PIN = 7;
+const char BRIGHTNESS = 127;
 
-const unsigned int cycleTime = 60000;
-unsigned long midiTimeout = 30000;
-const byte hueTime = 30; 
+const char BUF_SIZE = 64;
+const char hueTime = 30; 
 
 // fade-out rate. higher = longer, slower fades
-const byte fadeRate = 2;
+const char fadeRate = 2;
 
-byte fadeCount = 0;
-
-void setup() {
-  // write FastLED configuration data
-  // 
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-    .setCorrection(TypicalPixelString);
-
-  // set global brightness value
-  FastLED.setBrightness(BRIGHTNESS);
-}
+char fadeCount = 0;
+char rcvd[BUF_SIZE] = {0};
+char rcvd_pos = 0;
 
 // list of functions that will be displayed
 #define NUM_EFFECTS 4
 functionList effectList[] = {plasma, confetti, rider, slantBars};
 
-// Runs over and over until power off or reset
+void setup() {
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalPixelString);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  switch_to_effect(1);
+
+  Serial.begin(115200);
+}
+
 void loop() {
-  currentMillis = millis();  // save the current timer value
+  currentMillis = millis();
 
-  // run a fade effect too if the confetti effect or MIDI is running
-  if (effectList[currentEffect] == confetti) fadeCount++;
-  if (fadeCount >= fadeRate) {
-    fadeAll(1);
-    fadeCount = 0;
+  increment_hue();
+  increment_fade();
+  run_current_effect();
+
+  FastLED.show();
+
+  random16_add_entropy(analogRead(ENTROPY_PIN));
+}
+
+// handle incoming serial data
+void serialEvent() {
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\r'|| c == '\n') {
+      process_cmd(rcvd_pos);
+      rcvd_pos = 0;
+    } else {
+      rcvd[rcvd_pos] = c;
+      rcvd_pos = (rcvd_pos + 1) % BUF_SIZE;
+    }
   }
+}
 
-  FastLED.show();  // send the contents of the led memory to the LEDs
-
-  // switch to a new effect every cycleTime milliseconds
-  if (currentMillis - cycleMillis > cycleTime && autoCycle == true) {
-    cycleMillis = currentMillis;
-    // loop to start of effect list
-    if (++currentEffect >= NUM_EFFECTS) currentEffect = 0;
-    // trigger effect initialization when new effect is selected
-    effectInit = false;
+// process a finished serial command
+void process_cmd(char length) {
+  Serial.print("Received: ");
+  for (char i = 0; i < length; i++) {
+    Serial.print(rcvd[i]);
   }
+  Serial.println();
+}
 
-  // increment the global hue value every hueTime milliseconds
+// switch to a new effect every cycleTime milliseconds
+void switch_to_effect(char effect_num) {
+  currentEffect = effect_num;
+  effectInit = false;
+}
+
+// increment the global hue value every hueTime milliseconds
+void increment_hue() {
   if (currentMillis - hueMillis > hueTime) {
     hueMillis = currentMillis;
     hueCycle(1);  // increment the global hue value
   }
+}
 
-  // Run the currently selected effect every effectDelay milliseconds
+// Run the currently selected effect every effectDelay milliseconds
+void run_current_effect() {
   if (currentMillis - effectMillis > effectDelay) {
     effectMillis = currentMillis;
     effectList[currentEffect]();  // run the selected effect function
   }
+}
 
-  random16_add_entropy(analogRead(ENTROPY_PIN));
+// run a fade effect too if the confetti effect is running
+void increment_fade() {
+  if (effectList[currentEffect] == confetti) {
+    fadeCount++;
+    if (fadeCount >= fadeRate) {
+      fadeAll(1);
+      fadeCount = 0;
+    }
+  }
 }
